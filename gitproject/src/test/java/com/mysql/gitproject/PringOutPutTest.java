0095382e7c4623e625daa59134d0123a78a7074e
banti chowdhury <banti_chy@rediffmail.com>
2017-01-16 01:21:11 -0600

    vv

--- gitproject/src/test/java/com/mysql/gitproject/PringOutPutTest.java
+++ gitproject/src/test/java/com/mysql/gitproject/PringOutPutTest.java
@@ -4,6 +4,6 @@ public class PringOutPutTest {
 	
 	public static void main( String[] args )
     {
-        System.out.println( "Hello World!1" );
+        System.out.println( "Hello World!2" );
     }
 }
